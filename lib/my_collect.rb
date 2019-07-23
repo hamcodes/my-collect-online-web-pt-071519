ff1f38e4028aedfe5d0d785b6b8512e4cc6ff4a7
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 15:01:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-def my_collect(languages)
-  i = 0
-  collection = []
-  while i < collection.length
-  collection << yield(array[i])
-  i += 1
-  my_collect(languages) do |lang| lang.upcase
-     end
-     collection
-end
-end
+# def my_collect(languages)
+#   i = 0
+#   collection = []
+#   while i < collection.length
+#   collection << yield(array[i])
+#   i += 1
+#   my_collect(languages) do |lang| lang.upcase
+#     end
+#     collection
+# end
+# end
 
